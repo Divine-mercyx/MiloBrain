4166572f97f4c9fea78f53ef76f9e19e9ce2fd37
divine-mercyx <divinewilson766@gmail.com>
2025-09-20 07:56:01 +0100

    update: broke things down into functions

--- AI/route/ResponseRoute.js
+++ AI/route/ResponseRoute.js
@@ -1,8 +1,9 @@
 import {response, routerResponse} from "../controller/Response.js";
+import {POST} from "../response/route.js";
 
 export default async function responseRoute(fastify, options) {
     fastify.post('/response', {
-        handler: response
+        handler: POST
     })
     fastify.post('/router', {
         handler: routerResponse
